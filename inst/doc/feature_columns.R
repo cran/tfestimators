dd04af8b9f4150b62e46b5fbf374b4ff54cf7c4d
Tomasz Kalinowski <tomasz.kalinowski@rstudio.com>
2025-08-18 23:50:17 +0000

    version 1.9.3

--- inst/doc/feature_columns.R
+++ inst/doc/feature_columns.R
@@ -2,38 +2,38 @@
 knitr::opts_chunk$set(echo = TRUE, eval = FALSE)
 
 ## -----------------------------------------------------------------------------
-#  library(nycflights13)
-#  print(flights)
+# library(nycflights13)
+# print(flights)
 
 ## -----------------------------------------------------------------------------
-#  cols <- feature_columns(
-#    column_numeric("dep_time"),
-#    column_numeric("dep_delay")
-#  )
+# cols <- feature_columns(
+#   column_numeric("dep_time"),
+#   column_numeric("dep_delay")
+# )
 
 ## -----------------------------------------------------------------------------
-#  cols <- feature_columns(
-#    column_numeric("dep_time", "dep_delay")
-#  )
+# cols <- feature_columns(
+#   column_numeric("dep_time", "dep_delay")
+# )
 
 ## -----------------------------------------------------------------------------
-#  library(nycflights13)
-#  
-#  cols <- feature_columns(names = flights,
-#    column_numeric(ends_with("time"))
-#  )
+# library(nycflights13)
+# 
+# cols <- feature_columns(names = flights,
+#   column_numeric(ends_with("time"))
+# )
 
 ## -----------------------------------------------------------------------------
-#  cols <- with_columns(flights, {
-#    feature_columns(
-#      column_numeric(ends_with("time"))
-#    )
-#  })
+# cols <- with_columns(flights, {
+#   feature_columns(
+#     column_numeric(ends_with("time"))
+#   )
+# })
 
 ## -----------------------------------------------------------------------------
-#  cols <- with_columns(flights, {
-#    feature_columns(
-#      ends_with("time") ~ column_numeric(),
-#    )
-#  })
+# cols <- with_columns(flights, {
+#   feature_columns(
+#     ends_with("time") ~ column_numeric(),
+#   )
+# })
 
--- inst/doc/tensorboard.R
+++ inst/doc/tensorboard.R
@@ -3,9 +3,9 @@ library(tfestimators)
 knitr::opts_chunk$set(comment = NA, eval = FALSE)
 
 ## -----------------------------------------------------------------------------
-#  estimator(
-#      model_fn = model_fn,
-#      model_dir = "/tmp/test"
-#  ) %>% train(input_fn = input, steps = 100L)
-#  
+# estimator(
+#     model_fn = model_fn,
+#     model_dir = "/tmp/test"
+# ) %>% train(input_fn = input, steps = 100L)
+# 
 
